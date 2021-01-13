831eb532eb464dbe0513582fa21fe9ade24b2ff3
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 23:47:23 +0600

    post table seeder created

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
@@ -13,6 +15,19 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+        $title = $faker->sentence(45);
+
+        foreach (range(1, 25) as $index) {
+            Post::create([
+                "user_id"     => 1,
+                "category_id" => rand(1, 9),
+                'title' => $title,
+                'slug' => Str::slug($title),
+                "description" => $faker->paragraph(300),
+                "images"   => $faker->imageUrl(),
+                "status"      => boolValue(),
+            ]);
+        }
     }
 }
