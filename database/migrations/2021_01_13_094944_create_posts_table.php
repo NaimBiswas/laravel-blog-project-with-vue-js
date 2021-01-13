a780baed88dd0cda830d99f195ec852756611b09
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-14 00:02:56 +0600

    change unique for few time

--- database/migrations/2021_01_13_094944_create_posts_table.php
+++ database/migrations/2021_01_13_094944_create_posts_table.php
@@ -17,8 +17,8 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('category_id');
-            $table->string('title')->unique();
-            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('slug');
             $table->longText('description');
             $table->string('images');
             $table->string('status')->default(false);
