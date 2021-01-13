bd39aa1311667d984c81cdda106ddb5b6489795a
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 16:19:33 +0600

    create a user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'Naim Biswas',
+            'email' => 'nayeembiswas2@gmail.com',
+            'password' => Hash::make('nayeembiswas2@gmail.com'),
+        ]);
     }
 }
