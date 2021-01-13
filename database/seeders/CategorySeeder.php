d911558ab235334e0b057e90ea7ae082e699bbd5
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 16:06:05 +0600

    create a category seeder and some cat info

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +14,50 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        Category::create([
+            'name' => 'PHP',
+            'slug' => 'php',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'HTML',
+            'slug' => 'html',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'CSS',
+            'slug' => 'css',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'LARAVEL',
+            'slug' => 'laravel',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'WORDPRESS',
+            'slug' => 'wordpress',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'VUE-JS',
+            'slug' => 'vue-js',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'JAVASCRIPT',
+            'slug' => 'javascript',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'API',
+            'slug' => 'api',
+            'status' => true,
+        ]);
+        Category::create([
+            'name' => 'RESETFULL-API',
+            'slug' => 'resetfull-api',
+            'status' => true,
+        ]);
     }
 }
