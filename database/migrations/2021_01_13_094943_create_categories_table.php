23f94a3bc8e6fe1298402d80150759f125e845bf
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 16:00:38 +0600

    Create category table

--- database/migrations/2021_01_13_094943_create_categories_table.php
+++ database/migrations/2021_01_13_094943_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->tinyInteger('status')->default(0);
             $table->timestamps();
         });
     }
