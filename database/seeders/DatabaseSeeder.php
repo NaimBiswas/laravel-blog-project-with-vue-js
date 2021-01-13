665d8f8081c8ad9290d3797b62517d298fdf1177
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 16:11:32 +0600

    not model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(Category::class);
+        $this->call(CategorySeeder::class);
     }
 }
