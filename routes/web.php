6aad5173257ba06896a1d2ab4f5b6d3955c20a72
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 15:08:23 +0600

    remove home router

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,3 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-Route::get('/home', function () {
-    return view('layouts.admin.layouts');
-});
