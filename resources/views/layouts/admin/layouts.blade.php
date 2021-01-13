15fcda66a88604b284012736bbb6db8fc1d6b978
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 15:30:28 +0600

    remove ul li a

--- resources/views/layouts/admin/layouts.blade.php
+++ resources/views/layouts/admin/layouts.blade.php
@@ -118,29 +118,6 @@
                                         <i class="right fas fa-angle-left"></i>
                                     </p>
                                 </a>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="../../index.html"
-                                            class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Dashboard v1</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="../../index2.html"
-                                            class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Dashboard v2</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="../../index3.html"
-                                            class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Dashboard v3</p>
-                                        </a>
-                                    </li>
-                                </ul>
                             </li>
                             <li class="nav-item">
                                 <form action="{{ route('logout') }}"
