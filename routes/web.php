acfb305e7a67b398390d3de6b96ad6d5d87776a1
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-14 20:36:53 +0600

    create route gorup and projected it by checking auth is loged in or not

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-Route::get('/post', [PostController::class, 'index'])->name('post');
+Route::group(['prefix' => 'admin', 'middlewire' => 'auth'], function () {
+
+    Route::get('/post', [PostController::class, 'index'])->name('post');
+});
