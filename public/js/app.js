dbeb54e34ab7981d2b186e58335d699b0f8b3de5
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-14 21:53:23 +0600

    remove

--- public/js/app.js
+++ public/js/app.js
@@ -37384,7 +37384,7 @@ var staticRenderFns = [
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "container" }, [
       _c("div", { staticClass: "row justify-content-center" }, [
-        _c("div", { staticClass: "col-md-8" }, [
+        _c("div", { staticClass: "col-lg-12" }, [
           _c("div", { staticClass: "card" }, [
             _c("div", { staticClass: "card-header" }, [
               _vm._v("Example Component")
