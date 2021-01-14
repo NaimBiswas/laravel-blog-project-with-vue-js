b0f68a2edcc2a7bc27e1b395b8eaaa37f927f150
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-14 22:47:22 +0600

    removed prefix

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-Route::group(['prefix' => 'admin', 'middlewire' => 'auth'], function () {
+Route::group(['middlewire' => 'auth'], function () {
 
     Route::get('/post', [PostController::class, 'index'])->name('post');
 });
