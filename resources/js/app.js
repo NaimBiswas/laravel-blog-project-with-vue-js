deac6ae6e59b9179b82c77413cc7b1768b80483f
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-14 20:47:23 +0600

    import vuerouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-
+import router from 'vue-router';
+Vue.use(router);
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
