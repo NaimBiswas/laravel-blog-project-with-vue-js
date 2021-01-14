681ac12ba58cdcb78b2e8e29af021817700d93b2
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-14 23:27:12 +0600

    show cat info

--- resources/js/components/backend/category/category.vue
+++ resources/js/components/backend/category/category.vue
@@ -4,14 +4,13 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Blank Page</h1>
+                        <h1>Categories</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item">
-                                <a href="#">Home</a>
+                                <router-link to="/category" href="#">Category</router-link>
                             </li>
-                            <li class="breadcrumb-item active">Blank Page</li>
                         </ol>
                     </div>
                 </div>
