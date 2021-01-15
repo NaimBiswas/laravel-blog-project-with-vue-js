1f7c86be7d9c7ba96c6666d239cc424f027fb00d
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-15 10:12:05 +0600

    add form  for form validation

--- public/js/app.js
+++ public/js/app.js
@@ -38228,7 +38228,47 @@ var render = function() {
           _vm._m(1),
           _vm._v(" "),
           _c("form", { staticClass: "form-horizontal" }, [
-            _vm._m(2),
+            _c("div", { staticClass: "card-body" }, [
+              _c("div", { staticClass: "form-group row" }, [
+                _c(
+                  "label",
+                  {
+                    staticClass: "col-sm-2 col-form-label",
+                    attrs: { for: "catname" }
+                  },
+                  [_vm._v("Category Name:")]
+                ),
+                _vm._v(" "),
+                _c("div", { staticClass: "col-sm-10" }, [
+                  _c("input", {
+                    directives: [
+                      {
+                        name: "model",
+                        rawName: "v-model",
+                        value: _vm.name,
+                        expression: "name"
+                      }
+                    ],
+                    staticClass: "form-control",
+                    attrs: {
+                      type: "text",
+                      id: "catname",
+                      placeholder: "Enter Your Category Name",
+                      name: "name"
+                    },
+                    domProps: { value: _vm.name },
+                    on: {
+                      input: function($event) {
+                        if ($event.target.composing) {
+                          return
+                        }
+                        _vm.name = $event.target.value
+                      }
+                    }
+                  })
+                ])
+              ])
+            ]),
             _vm._v(" "),
             _c(
               "div",
@@ -38295,31 +38335,6 @@ var staticRenderFns = [
     return _c("div", { staticClass: "card-header" }, [
       _c("h3", { staticClass: "card-title pt-2" }, [
         _vm._v("Enter Your Category Name")
-      ])
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "card-body" }, [
-      _c("div", { staticClass: "form-group row" }, [
-        _c(
-          "label",
-          { staticClass: "col-sm-2 col-form-label", attrs: { for: "catname" } },
-          [_vm._v("Category Name:")]
-        ),
-        _vm._v(" "),
-        _c("div", { staticClass: "col-sm-10" }, [
-          _c("input", {
-            staticClass: "form-control",
-            attrs: {
-              type: "email",
-              id: "catname",
-              placeholder: "Enter Your Category Name"
-            }
-          })
-        ])
       ])
     ])
   }
