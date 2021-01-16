2d6ae3f5a53a129f8830d76e097d508a763c2c56
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-16 11:43:58 +0600

    reload issu fix

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::group(['middlewire' => 'auth'], function () {
 
     Route::get('/post', [PostController::class, 'index'])->name('post');
-    // Route::get('/{anypath}', [DashboardController::class, 'index'])->where('path', '.*');
     Route::post('/savecategory', [CategoryController::class, 'store'])->name('savecategory');
     Route::get('/get-categoy', [CategoryController::class, 'index'])->name('get-categories');
 });
+Route::get('/{anypath}', [DashboardController::class, 'index'])->where('path', '.*');
