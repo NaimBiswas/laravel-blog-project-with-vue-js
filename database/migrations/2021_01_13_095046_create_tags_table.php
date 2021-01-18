432d546ccfc9ff1be87658bba6eb8c0701c46245
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-18 15:51:27 +0600

    tag table created

--- database/migrations/2021_01_13_095046_create_tags_table.php
+++ database/migrations/2021_01_13_095046_create_tags_table.php
@@ -15,6 +15,9 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
