4217b709b8c8cfd08a6bdf323f1868c56cf2d861
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-18 23:16:03 +0600

    add sweetalert

--- public/js/app.js
+++ public/js/app.js
@@ -2608,7 +2608,22 @@ __webpack_require__.r(__webpack_exports__);
     isShowing: function isShowing() {
       return this.tags.length < 1;
     },
-    removeTag: function removeTag(id) {}
+    removeTag: function removeTag(id) {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: 'You will not be able to recover this imaginary file!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, keep it'
+      }).then(function (result) {
+        if (result.value) {
+          Swal.fire('Deleted!', 'Your imaginary file has been deleted.', 'success');
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+        }
+      });
+    }
   }
 });
 
--- resources/js/components/backend/tag/tag.vue
+++ resources/js/components/backend/tag/tag.vue
@@ -107,7 +107,28 @@ methods: {
         return this.tags.length < 1;
     },
     removeTag(id){
-
+        Swal.fire({
+  title: 'Are you sure?',
+  text: 'You will not be able to recover this imaginary file!',
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonText: 'Yes, delete it!',
+  cancelButtonText: 'No, keep it'
+}).then((result) => {
+  if (result.value) {
+    Swal.fire(
+      'Deleted!',
+      'Your imaginary file has been deleted.',
+      'success'
+    )
+  } else if (result.dismiss === Swal.DismissReason.cancel) {
+    Swal.fire(
+      'Cancelled',
+      'Your imaginary file is safe :)',
+      'error'
+    )
+  }
+})
     }
 },
 }
