369c808f8c2ed44aa82bd47a45e1490a34edc783
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-19 10:36:34 +0600

    create a action

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { get } from "jquery";
+import { postCss } from "laravel-mix";
 
 export default {
      state: {
         categoires: [],
-         tags:[],
+        tags: [],
+         poss: [],
     },
     actions: {
         getCategories(data) {
@@ -18,6 +20,13 @@ export default {
         getTags(tag) {
             axios.get('/index-tag').then((response) => {
                 tag.commit('fetchTags', response.data.tags);
+            }).catch((error) => {
+                toastr.warning(error);
+            });
+        },
+        getPosts() {
+            axios.get('/get-posts').then((response) => {
+                postCss.commit('fetchPosts', response.data.posts);
             }).catch((error) => {
                 toastr.warning(error);
             });
