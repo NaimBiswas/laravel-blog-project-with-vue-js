9f6ca394753de6194ae8cfb9c0ee4d417127f23e
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-19 20:58:44 +0600

    comments

--- routes/web.php
+++ routes/web.php
@@ -27,17 +27,19 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 Route::group(['middlewire' => 'auth'], function () {
 
-    Route::get('/get-posts', [PostController::class, 'index'])->name('post');
     Route::post('/savecategory', [CategoryController::class, 'store'])->name('savecategory');
     Route::get('/get-categoy', [CategoryController::class, 'index'])->name('get-categories');
     Route::delete('/remove-category/{id}', [CategoryController::class, 'destroy']);
     Route::get('/editecategory/{id}', [CategoryController::class, 'update']);
     Route::get('/fatch-category/{slug}', [CategoryController::class, 'edit'])->name('fatch-category');
     Route::post('/update-category/{slug}', [CategoryController::class, 'update'])->name('update-category');
+    #Tags
     Route::post('/store-tag', [TagController::class, 'store']);
     Route::get('/index-tag', [TagController::class, 'index']);
     Route::delete('/delete-tag/{slug}', [TagController::class, 'destroy']);
     Route::get('fetch-tag/{slug}', [TagController::class, 'edit']);
+    #Posts
+    Route::get('/get-posts', [PostController::class, 'index'])->name('post');
     Route::post('update-tag/{slug}', [TagController::class, 'update']);
 });
 Route::get('/{anypath}', [DashboardController::class, 'index'])->where('path', '.*');
